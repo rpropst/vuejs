5749a2555dd463a6b98a86324cd743560e1d97fc
Richard Propst <richard.propst@sentry.io>
2021-03-11 17:11:07 -0800

    update with router

--- src/main.js
+++ src/main.js
@@ -1,8 +1,25 @@
-import Vue from 'vue'
-import App from './App.vue'
+// import Vue from 'vue'
+// import App from './App.vue'
 
-Vue.config.productionTip = false
+// Vue.config.productionTip = false
+
+// new Vue({
+//   render: h => h(App),
+// }).$mount('#app')
+
+import Vue from 'vue'
+import App from './App'
+import router from './router'
+import * as Sentry from '@sentry/vue'
+
+Sentry.init({
+  Vue: Vue,
+  dsn: "https://1f91ba58ab884e0dbe236357ce72a5dd@o87286.ingest.sentry.io/5669173",
+});
 
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+  el: '#app',
+  // router,
+  components: { App },
+  template: '<App/>'
+})
